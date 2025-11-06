3812c6854ff973b96a69408c7ea50b01a24e3f9a
Ajith Narayanan <ajithnarayananka@gmail.com>
2025-11-06 11:29:22 +0530

    document analysis updated

--- model/models.py
+++ model/models.py
@@ -1,2 +1,13 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import Optional, List, Dict, Any, Union
+
+class Metadata(BaseModel):
+    Summary:List[str]
+    Title:str
+    Author:str
+    CreatedDate:str
+    LastModifiedDate:str
+    Publisher:str
+    Language:str
+    PageCount:Union[int,str] # Can be "Not Available"
+    SentimentTone:str
--- src/document_analyzer/data_analysis.py
+++ src/document_analyzer/data_analysis.py
@@ -1,10 +1,12 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
 class DocumentAnalyzer:
     """
@@ -12,6 +14,34 @@ class DocumentAnalyzer:
     Automatically logs all actions and support session-based organization.
     """
     def __init__(self):
-        pass
-    def analyze_metadata(self):
-        pass
+        self.log = CustomLogger().get_Logger(__name__)
+
+        try:
+            self.model_loader = ModelLoader()
+            self.llm = self.model_loader.load_llm()
+
+            self.parser = JsonOutputParser(pydantic_object= Metadata)
+            self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser,llm=self.llm)
+
+            self.prompt = prompt
+
+            self.log.info("Document Analyzer successfully Initialized.")
+
+        except Exception as e:
+            self.log.error(f'Error Initializing Document Analyzer:{e}')
+            raise DocumentPortalException("Error in Document Analyzer Initialization",e)
+
+    def analyze_document(self, document:str)-> dict:
+        """Extract the text from the document and extract structured metadata and summary"""
+        
+        chain = self.prompt | self.llm | self.fixing_parser
+
+        self.log.info("Rag chain is successfully initialized.")
+
+        response = chain.invoke({
+            "format_instructions":self.parser.get_format_instructions(),
+            "document_text": document
+            })
+        
+        self.log.info('Metadata extraction successful', keys=list(response.keys()))
+        return response
