d7de73b15763c51f9e672b8ade7f019d35e4a8b5
Ajith Narayanan <ajithnarayananka@gmail.com>
2025-11-12 14:55:26 +0530

    test and document compare code updated

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -18,25 +18,34 @@ class DocumentCompareLM:
         self.prompt = PROMPT_REGISTRY.get('document_comparison')
         self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
         self.fixing_parser = OutputFixingParser(parser=self.parser, llm=self.llm)
-        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.chain = self.prompt | self.llm | self.parser
 
         self.log.info("DocumentComparatorLM initialized with model and parser.")
-    def compare_document(self):
+    def compare_document(self, combine_docs: str)->pd.DataFrame:
         """
         Compare two documents and returns a structures comparison.
         """
         try:
-            pass
+            inputs = {
+                "combine_docs" : combine_docs,
+                "format_instructions" : self.parser.get_format_instructions()
+            }
+            self.log.info("Starting document comparison", inputs=inputs)
+            response = self.chain.invoke(inputs)
+            self.log.info("Document comparison completed", response=response)
+            return self._format_response(response)
         except Exception as e:
             self.log.error(f"Error in compare_documents: {e}")
             raise DocumentPortalException("An error occurred while comparing documents", sys)
 
-    def _format_response(self):
+    def _format_response(self,response_parsed : List[Dict])->pd.DataFrame:
         """
         Format the response from the LLM into a structured format.
         """
         try:
-            pass
+            df = pd.DataFrame(response_parsed)
+            self.log.info("Response formatted into DataFrame", dataframe = df)
+            return df
         except Exception as e:
             self.log.error("Error formatting response into DataFrame",error =str(e))
             raise DocumentPortalException("Error formatting response", sys)
