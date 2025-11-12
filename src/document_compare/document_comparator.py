a6ada41f83d1102b550864ad86a5099ab0e70712
Ajith Narayanan <ajithnarayananka@gmail.com>
2025-11-12 10:24:57 +0530

    code skeleton has been updated for document comparison

---
+++ src/document_compare/document_comparator.py
@@ -0,0 +1,19 @@
+import sys
+from dotenv import load_dotenv
+import pandas as pd
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from prompt.prompt_library import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentCompareLM:
+    def __init__(self):
+        pass
+    def compare_document(self):
+        pass
+
+    def _format_response(self):
+        pass
